341819514d57410ebc3ecf15a6ed20c5e03422a8
CowBoys1st <draharjo9@gmail.com>
2024-09-03 00:31:54 +0700

    feat: adding some elements

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,15 @@
-import Hero from '@/components/hero'
+import Hero from '@/components/Hero'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <>
+    <main className="main">
       <Hero />
-    </>
+      <div className='description'>
+        <p>Get started by editing&nbsp;
+          <code className='code'>src/app/page.tsx</code>
+        </p>
+      </div>
+    </main>
   )
 }
