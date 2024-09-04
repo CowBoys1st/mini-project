5e82c32053de16c6bf91f5c040c8cbbbfb813e33
CowBoys1st <draharjo9@gmail.com>
2024-09-05 00:57:15 +0700

    feat: add event controller

--- apps/api/src/controllers/event.controller.ts
+++ apps/api/src/controllers/event.controller.ts
@@ -4,15 +4,37 @@ import prisma from '@/prisma';
 
 export class EventController {
   async getEvents(req: Request, res: Response) {
-    const events = await prisma.event.findMany();
+    try {
+      const events = await prisma.event.findMany();
+      return res.status(200).send(events);
+    } catch (error){
+      return res.status(500).send({ message: 'Error fetching events', error })
+    }
 
-    return res.status(200).send(events);
   }
 
-  async createEvents(req:Request, res:Response) {
-    const {name, description, price, date, time, location, availableSeats, ticketType, category, isFree, attendees, promotions, reviews} = req.body
-
-    
+  async createEvent(req:Request, res:Response) {
+    try {
+      const {name, description, price, date, time, location, availableSeats, ticketType, category, isFree, organizerId } = req.body //{attendees, promotions, reviews}
+      const newEvent = prisma.event.create({
+        data: {
+          name,
+          description,
+          price,
+          date: new Date(date),
+          time,
+          location,
+          availableSeats,
+          ticketType,
+          category,
+          isFree,
+          organizerId,
+        },
+      })
+      return res.status(201).send(newEvent);
+    } catch (error) {
+      return res.status(500).send({ message: 'error creating event', error })
+    }
   }
 
 }
